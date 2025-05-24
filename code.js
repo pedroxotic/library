7b6e3fe6d9975fbe3cf09b41761b6ad19a87fffd
pedro <465.morales.pedro@gmail.com>
2025-05-24 13:04:13 -0600

    "table displays books"

--- code.js
+++ code.js
@@ -1,24 +1,49 @@
 
 let books = new Array();
+let table = document.getElementById('table');
 
 function Book (title,author, pages){
 this.title=title,
 this.author=author,
 this.pages=pages
-this.id=crypto.randomUUID()
+this.id=crypto.randomUUID();
 }
-
+let book1 = new Book('harry', 'jk', 100);
+console.log(book1);
 function addBookToLibrary (title,author, pages){
     let book = new Book(title,author, pages);
     books.push(book);
 }
-const body = document.body;
 addBookToLibrary('hobbit', 'Jk', 500);
 addBookToLibrary('df', 'Jk', 600);
+
+
 books.forEach(book => {
-  const p = document.createElement('p');
-  p.textContent = book.title;
-  body.appendChild(p);
+  const fila = document.createElement('tr');
+  fila.id = 'fila';
+  table.appendChild(fila);
+
+  // for each book agrega una celda a la fila
+  bookarr = new Array();
+  bookarr.push(book.title, book.author, book.pages);
+
+  console.log(bookarr);
+
+  
+  bookarr.forEach( (item,index) =>{
+    let celda = document.createElement('th');
+    celda.textContent = bookarr[index];
+    fila.appendChild(celda);
+  })
+  
+
+
+  /*
+  const celda = document.createElement('th');
+  celda.textContent = book.title;
+  fila.appendChild(celda);
+  */
+
 });
 
 
