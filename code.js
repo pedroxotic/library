a925cc7ffd901346610c4a0a552efa9f32eec98a
pedro <465.morales.pedro@gmail.com>
2025-05-29 12:48:58 -0600

    "commit to use on mac"

--- code.js
+++ code.js
@@ -1,43 +1,20 @@
 
 let books = new Array();
+let counter = 0;
 let table = document.getElementById('table');
 
+//Book constructor
 function Book (title,author, pages){
 this.title=title,
 this.author=author,
 this.pages=pages
 this.id=crypto.randomUUID();
 }
-let book1 = new Book('harry', 'jk', 100);
-console.log(book1);
+
 function addBookToLibrary (title,author, pages){
     let book = new Book(title,author, pages);
     books.push(book);
 }
-addBookToLibrary('hobbit', 'Jk', 500);
-addBookToLibrary('df', 'Jk', 600);
-
-
-books.forEach(book => {
-  const fila = document.createElement('tr');
-  fila.id = 'fila';
-  table.appendChild(fila);
-
-  // for each book agrega una celda a la fila
-  bookarr = new Array();
-  bookarr.push(book.title, book.author, book.pages);
-
-  console.log(bookarr);
-
-  
-  bookarr.forEach( (item,index) =>{
-    let celda = document.createElement('th');
-    celda.textContent = bookarr[index];
-    fila.appendChild(celda);
-  })
-  
-
-});
 
 let newbtn = document.getElementById("newbtn");
 let formulario = document.getElementById("modalOverlay");
@@ -49,4 +26,52 @@ let closemodal = document.getElementById("closeModal");
 closemodal.addEventListener('click', function(event){
   formulario.style.display = 'none';
 })
+
+const bookForm = document.getElementById("bookForm");
+
+bookForm.addEventListener('submit', function(event){
+event.preventDefault();
+ let booktitle = bookForm.elements['title'].value;
+  let bookauthor = bookForm.elements['author'].value;
+  let bookpages = bookForm.elements['pages'].value;
+  addBookToLibrary (booktitle,bookauthor, bookpages);
+  
+  displayBook(books, counter);
+  counter += 1;
+  console.log(books);
+})
+
+// hacer funcion displayBook
+
+function displayBook(books, counter){
+ const fila = document.createElement('tr'); // Create a new table row
+  fila.id = 'fila';
+  fila.dataset.id=counter;
+  table.appendChild(fila);
+
+ let bookarr = [books[counter].title, books[counter].author, books[counter].pages];
+
+ bookarr.forEach((item, index) => {
+    let celda = document.createElement('th');
+    celda.textContent = bookarr[index];
+    fila.appendChild(celda);
+  });
+    let celdaDelete = document.createElement('button');
+    celdaDelete.textContent = "x";
+    celdaDelete.dataset.id=counter;
+    fila.appendChild(celdaDelete);
+
+    celdaDelete.addEventListener('click', function(event){
+      // necesita sacarlo del dom y sacarlo de el array 
+      // necesito saber que fila se apreto, esa # numero de fila usarlo
+      // para  eliminar el libro del array
+      
+     books.slice(parseInt(celdaDelete.dataset.id), parseInt(celdaDelete.dataset.id));
+     console.log(books);
+
+    } )
+
+  } 
+
+  // necesito saber que delete boton aprete 
 
